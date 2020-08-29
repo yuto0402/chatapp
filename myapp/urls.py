0127d053f0528f8a7a9cf734de3916f14fd9deea
ISSEI-HAYASHI <issei884money@gmail.com>
2020-08-29 14:07:09 +0900

    [views],[urls]追加

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('signup', views.signup, name='signup'),
+    path('signup', SignUp.as_view(), , name='signup'),
+    path('login', views.login, name='login'),
+    path('friends', views.friends, name='friends'),
+    path('talk_room', views.talk_room, name='talk_room'),
+    path('setting', views.setting, name='setting'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import SignupForm
 
 # Create your views here.
+
+def index(request):
+    return render (request, "myapp/index.html")
+
+class SignUp(CreateView):
+    form_class = SignupForm
+    template_name = "myapp/signup.html" 
+    # success_url = reverse_lazy('top')
+
+    def form_valid(self, form):
+        user = form.save() # formの情報を保存
+        login(self.request, user) # 認証
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url()) # リダイレクト
+
+# def signup(request):
+#     form_class = SignupForm
+#     user = form.save() # formの情報を保存
+#     return render (request, "myapp/signup.html")
+
+def login(request):
+    return render (request, "myapp/login.html")
+
+def friends(request):
+    return render (request, "myapp/friends.html")
+
+def talk_room(request):
+    return render (request, "myapp/talk_room.html")
+
+def setting(request):
+    return render (request, "myapp/setting.html")
