cc4bfe90e5557534c608e33e179dc5ac2f77a83d
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2020-09-01 05:49:28 +0900

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-# class User(models.Model):
-#     name = models.CharField(max_length=20)
-#     mail = models.EmailField(max_length=100)
-#     password = models.CharField(max_length=50)
