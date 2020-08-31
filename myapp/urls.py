55ef4a8705fe12a258db34978ac5290b3e71bfa1
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2020-09-01 06:46:25 +0900

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup_view, name='signup_view'),
-    path('login', views.Login.as_view(), name='login_view'),
+    path('login', views.login_view, name='login_view'),
     path('friends', views.friends, name='friends'),
     path('talk_room', views.talk_room, name='talk_room'),
     path('setting', views.setting, name='setting'),
