ceb210c260582e2886aa07da3f950bb1411473a1
takumi <taku_kaji@ezweb.ne.jp>
2020-09-01 06:59:19 +0900

    デバック　master branch

--- myapp/views.py
+++ myapp/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import redirect, render
 
 
 def index(request):
-    return render (request, "myapp/index.html")
+    return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request,"myapp/signup.html")
+    return render(request, "myapp/signup.html")
 
 def login_view(request):
-    return render (request, "myapp/login.html")
+    return render(request, "myapp/login.html")
 
 def friends(request):
-    return render (request, "myapp/friends.html")
+    return render(request, "myapp/friends.html")
 
 def talk_room(request):
-    return render (request, "myapp/talk_room.html")
+    return render(request, "myapp/talk_room.html")
 
 def setting(request):
-    return render (request, "myapp/setting.html")
+    return render(request, "myapp/setting.html")
