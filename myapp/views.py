8dc28bbedef435e6e6a27d4e581ad4422601a52d
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2020-09-01 02:53:01 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,8 @@
 from django.shortcuts import redirect, render
-from .forms import SignupForm
 
-# Create your views here.
 
 def index(request):
     return render (request, "myapp/index.html")
-
-class SignUp(CreateView):
-    form_class = SignupForm
-    template_name = "myapp/signup.html" 
-    # success_url = reverse_lazy('top')
-
-    def form_valid(self, form):
-        user = form.save() # formの情報を保存
-        login(self.request, user) # 認証
-        self.object = user 
-        return HttpResponseRedirect(self.get_success_url()) # リダイレクト
-
-# def signup(request):
-#     form_class = SignupForm
-#     user = form.save() # formの情報を保存
-#     return render (request, "myapp/signup.html")
 
 def login(request):
     return render (request, "myapp/login.html")
