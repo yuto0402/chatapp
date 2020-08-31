391fea5fa919e220b502d7dd00690925a9efada0
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2020-09-01 06:45:14 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import redirect, render
 def index(request):
     return render (request, "myapp/index.html")
 
+def signup_view(request):
+    return render(request,"myapp/signup.html")
+
 def login(request):
     return render (request, "myapp/login.html")
 
