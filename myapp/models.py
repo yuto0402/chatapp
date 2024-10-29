3d25e182e6553158f95e88e97c035f0e8bb0cd37
yuto0402 <yamada.yuto.54x@st.kyoto-u.ac.jp>
2024-10-29 19:10:21 +0900

    commit message

--- myapp/models.py
+++ myapp/models.py
@@ -11,14 +11,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return str(self.username)
-
-class Friend(models.Model):
-    class Meta:
-        verbose_name = "Friend"
-        verbose_name_plural = "Friend"
-
-    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.CASCADE, related_name='User', null=True)
-    friend = models.ForeignKey(CustomUser, verbose_name='友達', on_delete=models.CASCADE, related_name='Friend', null=True)
     
 class TalkRoom(models.Model):
     sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender', null=True)
